2439559c5e124316e8c1cb1965fde14419816d83
viezahrazuhdi <arpianti88@guru.smp.belajar.id>
2026-07-20 21:14:47 +0800

    Update app.js

--- tbn8/app.js
+++ tbn8/app.js
@@ -2614,7 +2614,7 @@ async function cetakJurnalMengajar() {
   htmlPrint += '</div>';
 
   htmlPrint += '<table><thead>';
-  htmlPrint += '<tr><th width="5%">No</th><th width="15%">Hari, Tanggal</th><th width="10%">Kelas</th><th width="15%">Mapel (Jam)</th><th width="25%">Topik</th><th width="10%">Catatan</th><th width="20%">Presensi Siswa</th></tr>';
+  htmlPrint += '<tr><th width="5%">No</th><th width="15%">Hari, Tanggal</th><th width="10%">Kelas</th><th width="15%">Mapel (Jam)</th><th width="20%">Topik</th><th width="20%">Catatan</th><th width="15%">Presensi Siswa</th></tr>';
   htmlPrint += '</thead><tbody>';
 
   for (let i = 0; i < dataCetak.length; i++) {
